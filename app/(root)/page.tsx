2ad6d349cc445472bcba743fa906b0d9ca8391d8
sexxyrexxy <rexlim2003@gmail.com>
2025-08-26 14:11:32 +1000

    landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,26 +6,32 @@ import { Button } from "@/components/ui/button";
 const Homepage = () => {
   return (
     <>
-      <div className="w-full h-[500px] relative">
+      <div className="h-[500px] relative">
         <Image
           src="/images/landing-page-image.jpg"
           alt="image mushroom"
-          width={1920}
-          height={500}
-          className="w-full h-[500px] object-cover"
+          fill
+          className="object-cover"
           sizes="100vw"
+          priority
         />
 
         {/* Overlay text */}
-        <div className="absolute inset-0 flex flex-col items-start justify-center text-white text-center wrapper">
+        <div className="absolute inset-0 flex flex-col items-start justify-center text-white wrapper">
           <h1 className="font-bold text-5xl drop-shadow-lg">Mycelium</h1>
           <p className="text-2xl mt-4 drop-shadow-lg">
             A look into the mushroom mind
           </p>
         </div>
       </div>
-      <div className="wrapper">
-        <MushroomCard />
+      <div className="p-5">
+        <div className=" grid grid-cols-4 gap-4">
+          <MushroomCard />
+          <MushroomCard />
+          <MushroomCard />
+          <MushroomCard />
+          <MushroomCard />
+        </div>
       </div>
     </>
   );
--- components/shared/muhsroom/mushroom-card.tsx
+++ components/shared/muhsroom/mushroom-card.tsx
@@ -5,24 +5,29 @@ import Link from "next/link";
 
 const MushroomCard = () => {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader className="p-0 items-center">
-        <Link href={`/product/`}>
+    <Card className="w-full max-w-sm pt-0">
+      <CardHeader className="!p-0 relative h-60">
+        <Link href="/product/" className="block w-full h-full">
           <Image
             src="/images/landing-page-image.jpg"
             alt="Oyster"
-            width={300}
-            height={300}
-          ></Image>
+            fill
+            className="object-cover rounded-t-xl"
+            sizes="(max-width: 640px) 100vw, 300px"
+          />
         </Link>
       </CardHeader>
+
       <CardContent className="p-4 grid gap-4">
-        <Link href={`/product/`} className="grid gap-4">
-          <h2 className="text-sm font-medium">Oyster</h2>
+        <Link href="/product/" className="grid gap-4">
+          <div>
+            <h2 className="text-sm font-medium">Oyster</h2>
+            <div className="text-xs">Pleurotus ostreatus</div>
+          </div>
           <div className="flex-between gap-4">
             <p>Oyster mushroom grows in rainforests etc.</p>
             <Button asChild>
-              <Link href="/profile">Read more</Link>
+              <Link href="/profile">Grow</Link>
             </Button>
           </div>
         </Link>
