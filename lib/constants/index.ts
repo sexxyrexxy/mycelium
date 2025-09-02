23c290019a62bdd67108e78455813d77cdefcdff
medley.co <195531267+medley-co@users.noreply.github.com>
2025-09-02 17:01:53 +1000

    Myco Lab change

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,6 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Mycelium';
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A digital museums built for mushrooms';
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Myco Lab";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A digital museums built for mushrooms";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
